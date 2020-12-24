16545f549c5130e5ad8b67129fd2b6cc73443fd6
kwj5584 <kvvj5584@gmail.com>
2020-12-24 15:14:55 +0900

    axios, concurrently, proxy 의존성 추가

--- client/src/App.js
+++ client/src/App.js
@@ -14,18 +14,15 @@ function App() {
   return (
     <Router>
       <div>
-        
-        
-        <Switch>
-          <Route exact path="/" component={LandingPage} /> {/* 방법1 */}
+        <Route exact path="/" component={LandingPage} /> {/* 방법1 */}
 
-          <Route exact path="/login">
-            <LoginPage />                         {/* 방법2 */}
-          </Route>
-          <Route exact path="/register">
-            <RegisterPage />
-          </Route> 
-        </Switch>
+        <Route exact path="/login">
+          <LoginPage />                         {/* 방법2 */}
+        </Route>
+        <Route exact path="/register">
+          <RegisterPage />
+        </Route> 
+        
       </div>
     </Router>
   );
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
 const port = 5000;
+
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const {User} = require('./models/User');
@@ -11,6 +12,7 @@ const {auth} = require('./middleware/auth')
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
 dotenv.config();
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_USERPASSWORD}@cluster0.2zeyh.mongodb.net/<dbname>?retryWrites=true&w=majority`,{
