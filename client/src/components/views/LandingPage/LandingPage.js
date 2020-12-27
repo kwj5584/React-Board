ca525713b60d38346b9ac36ee95bda822b051c56
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2020-12-27 15:44:02 +0900

    로그아웃 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,29 @@
 import React,{useEffect} from 'react'
 import axios from 'axios';
+import {withRouter} from 'react-router-dom'
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get('/api/hello')
     .then( res => console.log(res))
     }, [])
+    // const isLogin = props.location.state.userInfo.loginSuccess
+    // console.log('userInfo :', isLogin)
+    const onLogoutHandler = ()=>{
+      axios.post('/api/users/logout')
+      .then(res=>{
+        console.log('프론트 로그아웃버튼 클릭',res)
+        if(res.data.success){
+          alert('로그아웃 성공')
+          props.history.push('/login')
+        }else{
+          alert('로그아웃 실패')
+        }
+      })
+    }
+    const onLoginHandler = ()=>{
+      props.history.push('/login')
+    }
   
   return (
     <div style={{
@@ -13,8 +31,14 @@ function LandingPage() {
       width:"100%", height: '100vh'
     }}>
       <h2>시작페이지</h2>
+      <button onClick={onLogoutHandler}>로그아웃</button>
+      {/* { isLogin ?<button onClick={onLogoutHandler}>로그아웃</button>
+      : <button onClick={onLoginHandler}>로그인</button>
+      } */}
     </div>
   )
   }
 
-export default LandingPage
+export default withRouter(LandingPage)
+
+export default withRouter(LandingPage)
