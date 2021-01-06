1459232a2a199b6adafeaff1f83e3759066b8662
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-06 17:47:08 +0900

    유저관련 api 분리 및 게시판 create, read api 추가

---
+++ server/Api/Board/boardApi.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+router.use(cors())
+router.use(bodyParser.json())
+const {Board} = require('../../models/Board');
+
+
+router.post('/add', (req,res)=>{
+
+    const title = req.body.title;
+    const userName = req.body.userName;
+    const contents = req.body.contents
+    
+    try{
+        const board = new Board({title,userName,contents});
+        board.save();
+        res.status(201).json({
+            submit:true
+        })
+    }catch(err){
+        console.log(err);
+    }
+})
+router.get('/getList',(req,res)=>{
+    Board.find({},{_id:1,title:1,userName:1,contents:1},(err,boardList)=>{
+        if(err) return res.status(500).json({error:err});
+        if(!boardList) return res.status(404).json({message : 'boardList not found'})
+
+        res.send(boardList)
+    })
+
+})
+
+
+module.exports = router;
