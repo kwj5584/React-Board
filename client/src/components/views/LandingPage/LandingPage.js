1f70623f5cf97cca46166b6a6be06040038b1cc4
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-06 17:51:31 +0900

    로그인 안해놓으면 글 작성 못하게 변경

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,6 @@
 import React,{useEffect, useState} from 'react'
 import axios from 'axios';
+import {Button} from 'react-bootstrap'
 import {withRouter} from 'react-router-dom'
 import List from '../List/List'
 
@@ -7,20 +8,35 @@ import List from '../List/List'
 function LandingPage(props) {
   
   const [name,setName] = useState('');
+  const [isLogin,setIsLogin] = useState('false')
+
   useEffect(()=>{
     axios.get('api/users/auth')
     .then((res)=>{
-      console.log(res);
+      console.log('랜딩페이지: ',res);
       setName(res.data.name)
+      setIsLogin(res.data.isAuth)
     })
   },[])
+  const listAddPage =()=>{
+    if(!isLogin){
+      alert('로그인 먼저하세요')
+      props.history.push('/login')
+    }else{
+    props.history.push({
+        pathname: '/listAdd',
+        state:{name: name}})
+      }
+}
   
   return (
     <div style={{
       display:'flex', justifyContent:'center', alignItems:'center',
       width:"100%", height: '100vh'
     }}>
-      <List name={name}/>
+      <List/>
+      <br/>
+      <Button onClick={listAddPage}>글 작성</Button>
       
     </div>
   )
