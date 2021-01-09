ad161b50234dabad1ae91237f5366e76ea401959
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-09 16:34:14 +0900

    게시글 삭제 리듀서함수 추가

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,35 +1,40 @@
 import {
-    LOGIN_USER, REGISTER_USER, AUTH_USER, LIST_ADD, GET_LIST, GET_DETAIL,LIST_UPDATE
+    LOGIN_USER, REGISTER_USER, AUTH_USER, 
+    LIST_ADD, GET_LIST, GET_DETAIL,LIST_UPDATE, LIST_DELETE
 } from '../_actions/types';
 
 const Reducer = (state = {},action) =>{
     switch (action.type){
         case LOGIN_USER:
             return{...state, loginSuccess: action.payload}
-            break;
+            // break;
 
         case REGISTER_USER:
             return {...state, register : action.payload}
-            break;
+            // break;
 
         case AUTH_USER:
             return{ ...state, userData : action.payload}
-            break;
+            // break;
 
         case LIST_ADD:
             return{ ...state, listData : action.payload}      
-            break; 
+            // break; 
 
         case GET_LIST:
             return{ ...state, getList: action.payload}
-            break;
+            // break;
 
         case GET_DETAIL:
             return{ ...state, getDetail: action.payload}
-            break;
+            // break;
 
         case LIST_UPDATE:
             return{ ...state, updateSuccess: action.payload}
+        
+        case LIST_DELETE:
+            return{ ...state, deleteSuccess: action.payload }
+
         default:
             return state;
     }
