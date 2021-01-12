5a7c9d8c4ee92c3751331be23862ec0e2f587210
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-12 16:59:26 +0900

    검색필터 리듀서함수 추가

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -7,28 +7,23 @@ const Reducer = (state = {},action) =>{
     switch (action.type){
         case LOGIN_USER:
             return{...state, loginSuccess: action.payload}
-            // break;
 
         case REGISTER_USER:
             return {...state, register : action.payload}
-            // break;
-
+          
         case AUTH_USER:
             return{ ...state, userData : action.payload}
-            // break;
+            
 
         case LIST_ADD:
             return{ ...state, listData : action.payload}      
-            // break; 
-
+            
         case GET_LIST:
             return{ ...state, getList: action.payload}
-            // break;
-
+            
         case GET_DETAIL:
             return{ ...state, getDetail: action.payload}
-            // break;
-
+           
         case LIST_UPDATE:
             return{ ...state, updateSuccess: action.payload}
         
