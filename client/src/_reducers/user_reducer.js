b4a04ded6b640374415fdbd394bb93631220f51e
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-12 16:36:54 +0900

    검색필터 리듀서함수 추가(미완)

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,6 +1,6 @@
 import {
     LOGIN_USER, REGISTER_USER, AUTH_USER, 
-    LIST_ADD, GET_LIST, GET_DETAIL,LIST_UPDATE, LIST_DELETE
+    LIST_ADD, GET_LIST, GET_DETAIL,LIST_UPDATE, LIST_DELETE, FIND_TITLE, FIND_USERNAME
 } from '../_actions/types';
 
 const Reducer = (state = {},action) =>{
@@ -35,6 +35,11 @@ const Reducer = (state = {},action) =>{
         case LIST_DELETE:
             return{ ...state, deleteSuccess: action.payload }
 
+        case FIND_TITLE:
+            return{...state, getList: action.payload}
+        
+        case FIND_USERNAME:
+            return{...state, getList: action.payload}
         default:
             return state;
     }
