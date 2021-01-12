39259a98f68ef08c7d3f9bc2ed4c2e1981816b16
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-12 16:59:55 +0900

    검색필터 액션함수 추가

--- client/src/_actions/types.js
+++ client/src/_actions/types.js
@@ -2,9 +2,10 @@ export const LOGIN_USER = 'login_user';
 export const REGISTER_USER = 'register_user';
 export const AUTH_USER = 'auth_user';
 export const LIST_ADD = 'list_add';
-export const GET_LIST = 'get_list'
-export const GET_DETAIL = 'get_detail'
-export const LIST_UPDATE = 'list_update'
-export const LIST_DELETE = 'list_delete'
-export const FIND_TITLE = 'find_title'
-export const FIND_USERNAME = 'find_username'
+export const GET_LIST = 'get_list';
+export const GET_DETAIL = 'get_detail';
+export const LIST_UPDATE = 'list_update';
+export const LIST_DELETE = 'list_delete';
+export const FIND_TITLE = 'find_title';
+export const FIND_USERNAME = 'find_username';
+
