b07acdbf2c05c5950cdb22020c38d9d206a08495
kwj5584 <74160659+kwj5584@users.noreply.github.com>
2021-01-17 14:07:26 +0900

    회원가입 captcha 등록

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -2,15 +2,16 @@ import React,{useState} from 'react'
 import {withRouter} from 'react-router-dom'
 import {useDispatch} from 'react-redux';
 import {registerUser} from '../../../_actions/user_action'
+import ReCAPTCHA from 'react-google-recaptcha'
 
 function RegisterPage(props) {
-
   const dispatch = useDispatch();
   
   const [Name, setName] = useState("")
   const [Email, setEmail] = useState("")
   const [Password,setPassword] = useState("")
   const [ConfirmPassword, setConfirmPassword] = useState("")
+  const [captcha, setCaptcha] = useState('')
 
   const onNameHandler = (event)=>{
     setName(event.currentTarget.value)
@@ -25,12 +26,18 @@ function RegisterPage(props) {
     setConfirmPassword(event.currentTarget.value)
   }
 
+  const captchaOnChange =  (value)=>{
+    setCaptcha(value);
+  }
   const onSubmitHandler = (event) =>{
     event.preventDefault();
   
 
   if(Password !== ConfirmPassword){
     return alert('비밀번호와 비밀번호 확인은 같아야 합니다.');
+  }
+  else if(!captcha){
+    return alert('로봇이 아님을 증명해야합니다.')
   }
   else{
     let body = {
@@ -52,7 +59,7 @@ function RegisterPage(props) {
   return (
     <div style={{
       display:'flex', justifyContent:'center', alignItems:'center',
-      width:"100%", height: '100vh'
+      width:"100%", height: '50vh'
     }}>
       <form style={{display:'flex', flexDirection:'column'}}>
 
@@ -68,6 +75,11 @@ function RegisterPage(props) {
         <label>Password : </label>
         <input type='password' value={ConfirmPassword} onChange={onConfirmPasswordHandler}/>
 
+        <br/>
+        <ReCAPTCHA
+          sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+          onChange={captchaOnChange}
+        />
         <br/>
         <button onClick={onSubmitHandler}>회원가입</button>
       </form>
